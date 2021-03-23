56dbabbbd04430a6b4545e397788b40949a00483
RyanRau <ryanzrau@gmail.com>
2021-03-23 14:00:37 -0500

    started route for mass user creation

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import current_user, UserList, UserInformation
+from .views import current_user, UserList, UserInformation, FileUploadView
 
 urlpatterns = [
     path('current_user/', current_user.as_view()),
     
     path('users/', UserList.as_view()),
-    path('users/<int:pk>/', UserInformation.as_view())
+    path('users/<int:pk>/', UserInformation.as_view()),
+    path('users/upload', FileUploadView.as_view())
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,12 @@
-from django.http import HttpResponseRedirect
-from django.contrib.auth.models import User
+import os
+import pandas as pd
+
 from rest_framework import permissions, status
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.exceptions import ParseError
+
 from .serializers import UserSerializer, UserSerializerWithToken
 from .models import ExtendedUser
 
@@ -38,3 +41,48 @@ class UserInformation(APIView):
         user = ExtendedUser.objects.get(pk=pk)   
         serializer = UserSerializer(user)
         return Response(serializer.data)
+
+
+class FileUploadView(APIView):
+    permission_classes = (permissions.AllowAny,)
+    parser_classes = ( MultiPartParser, FormParser, )
+
+    def post(self, request):
+        if 'file' not in request.data:
+            raise ParseError("No file provided")
+
+        csv_file = request.data['file']
+
+        ext = os.path.splitext(csv_file.name)[1].lower()
+        print(ext)
+        if ext != '.csv':
+            raise ParseError("Improper file type provided")
+
+        df = pd.read_csv(csv_file)
+
+        DEFAULT_PASSWORD = 'UTCH123$'
+        results = {
+            'added_users': [],
+            'errors': []
+        }
+
+        ####################### 
+        # Replace this bit...
+
+        print(df)
+
+        if not ExtendedUser.objects.filter(username='rrau').exists():
+            ExtendedUser.objects.create(username='rrau',
+                                        first_name='ryan',
+                                        last_name='rau',
+                                        classification=0,
+                                        email='ryanrau@uark.edu',
+                                        password=DEFAULT_PASSWORD)
+            results['added_users'].append('rrau')
+        else:
+            ExtendedUser.objects.filter(username='rrau').delete()
+            results['errors'].append('failed to add rrau')
+
+        #######################
+
+        return Response(results, status=status.HTTP_200_OK)
